f967c5d0980e260ffb5e739fe0fb87dc1e9fb35b
Bowen Zhang <bzhang193@gmail.com>
2020-08-27 21:32:15 -0400

    updated code to create train/test sets

--- create_melspectrograms.py
+++ create_melspectrograms.py
@@ -5,22 +5,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 from re import findall
 
-# set up the parameters for conversion to mel spectrogram (should I include these into the function or class I am going to create to generate spectrograms?)
-n_fft = 2048
-hop_length = 512
-n_mels = 64
-f_min = 20
-f_max = 8000
-sample_rate = 16000
-
 # Create a function to convert wav files to images of melspectrograms in a given folder
-def create_melspec(fold, MAX=10000):
+def create_melspec(fold, train=9000, test=1000, n_fft = 2048, hop_length = 512, n_mels = 64, f_min = 20, f_max = 8000, sample_rate = 16000):
     '''
-    Function to create spectrograms for all .wav files under a gven language folder
+    Function to create mel-scaled spectrograms for all .wav files under a given language folder.
+    Mel-scaled spectrograms (melspectrograms) are spectrograms with frequency converted to the mel-scale (a non-linear transformation)
 
     Inputs:
     fold = the wav files folder location RELATIVE to the currently working directory
-    MAX = the maximum number of melspectrograms / data points to generate
+    train = the number of training samples to generate
+    test = the number of test samples to generate
+
+    technical inputs:
+    n_fft = 2048            # number of frames for the fast fornier transformation
+    hop_length = 512        # window (hop length) of frames to analyse at a time
+    n_mels = 64             # the number of mel filters to apply
+    f_min = 20              # minimum frequency
+    f_max = 8000            # max frequency
+    sample_rate = 16000     # rate per second the time is audio is sampled. 16000 ~==
 
     Output:
     A spectrogram image for each .wav file under a folder: 'spectrograms' and respective subfolders for each language
@@ -30,21 +32,41 @@ def create_melspec(fold, MAX=10000):
     - if you used the bash script to download and extract the data, this should be automatically set up correctly
 
     Example:
-    create_melspec(fold='en_wav', MAX=7000)
-        > returns: spectrograms/en_data/ <image of melspectrogram>.png ... for every wav file in 'en_wav'
+    create_melspec(fold='en_wav', train=9000, test=1000)
+        > returns: <train/test>/<language>/ <image of melspectrogram>.png ... for every wav file in 'en_wav' with a train/test split of 0.9/0.1
     '''
 
-    print(f'Processing spectrograms in {fold}')
+    print(f'Creating melspectrograms in {fold}')
 
     # get list of files under the given folder
     wav_files = os.listdir(path=fold)
 
-    # creates subdirectories for the output of melspectrogram images
-    spectrogram_path = f'spectrograms/{fold.replace("_wav", "_data")}'
-    if os.path.isdir(spectrogram_path):
+    # creates subdirectories for the output of melspectrogram images (train/test split)
+    if 'en' in fold:
+        spectrogram_path_train = f'vox_lang/train/{fold.replace(fold, "English")}'
+        spectrogram_path_test = f'vox_lang/test/{fold.replace(fold, "English")}'
+    elif 'fr' in fold:
+        spectrogram_path_train = f'vox_lang/train/{fold.replace(fold, "French")}'
+        pectrogram_path_test = f'vox_lang/test/{fold.replace(fold, "French")}'
+    elif 'es' in fold:
+        spectrogram_path_train = f'vox_lang/train/{fold.replace(fold, "Spanish")}'
+        pectrogram_path_test = f'vox_lang/test/{fold.replace(fold, "Spanish")}'
+    elif 'de' in fold:
+        spectrogram_path_train = f'vox_lang/train/{fold.replace(fold, "German")}'
+        pectrogram_path_test = f'vox_lang/test/{fold.replace(fold, "German")}'
+    ### you can keep adding to this line for all languages you choose to include ###
+
+    # create the path for the spectrograms if it doesn't exist already
+    if os.path.isdir(spectrogram_path_train):
         pass
     else:
-        os.makedirs(spectrogram_path)
+        os.makedirs(spectrogram_path_train)
+
+    # do the same for test
+    if os.path.isdir(spectrogram_path_test):
+        pass
+    else:
+        os.makedirs(spectrogram_path_test)
 
     # convert wav to melspectrograms and saves the image
     counter = 0
@@ -72,7 +94,12 @@ def create_melspec(fold, MAX=10000):
         # extract the speaker from filename and rename with autoincrement key
         speaker = findall(r'(?<=wav_).*?[._-]', audio_file)
         new_name = audio_file.replace(audio_file, f'{speaker[0]}{counter}.png')
-        filename  = f'{spectrogram_path}/{new_name}'
+
+        # decide where to store the files (train or test)
+        if counter >= train:
+            filename  = f'{spectrogram_path_test}/{new_name}'
+        else:
+            filename  = f'{spectrogram_path_train}/{new_name}'
 
         # save the output image
         plt.savefig(filename, dpi=400, bbox_inches='tight',pad_inches=0)
@@ -81,11 +108,12 @@ def create_melspec(fold, MAX=10000):
         counter+=1
 
         # cap the max number of data for each class to have a balanced distribution
-        if counter >= MAX:
+        if counter >= (train+test):
             break
+
 
 # create our spectrograms
 lang_folders = ['en_wav','fr_wav','es_wav','de_wav']
 
 for i in lang_folders:
-    create_melspec(i, MAX=6500)
+    create_melspec(i, train=9000, test=1000)
--- voxforge_dl.sh
+++ voxforge_dl.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#### This script will download, extract, and move wav files from the voxforge site ####
 
 #### NOTE: Make sure you set your directory to where you want to download the files before running script ####
 #### This script creates the data directory and downloads plus extracts all raw wav files in the following langauges ####
+# List of languages to download. Based on the URL of voxforge.
 LanguageArray=("en" "fr" "es" "de")
 
 # creates a new folder in your current working directory
@@ -9,30 +11,29 @@ LanguageArray=("en" "fr" "es" "de")
 cd vox_lang;
 rootdir=$(pwd)
 
-# download 1gb for each language and wait 5 seconds per download to not overload server
+### Download files for each language and wait 5 seconds per download to not overload server ####
 for lang in ${LanguageArray[*]}; do
-	# Use wget instead if you want to just download from top to bottom or everything (remove the -Q limit if so)
-	# wget -r -np -Q1024m -nc -w3 R "index.html*" -P /$lang http://www.repository.voxforge1.org/downloads/$lang/Trunk/Audio/Main/16kHz_16bit/
 
-	# create folder for each language and change to that directory
-	[[ ! -d $lang ]] && mkdir $lang; cd $lang;
+	#### Use wget instead if you want to just download from top to bottom or everything (-Q limits the max size downloaded per class) ####
+	wget -r -np -Q1024m -nc -w3 R "index.html*" -P /$lang http://www.repository.voxforge1.org/downloads/$lang/Trunk/Audio/Main/16kHz_16bit/
 
-	# download each file on the list
-	while IFS="," read filename
-	do
-	    curl http://www.repository.voxforge1.org/downloads/$lang/Trunk/Audio/Main/16kHz_16bit/$filename
-	done < "${lang}_list.csv"
-
-	cd $rootdir;
+	#### Use the code below if you want to download a randomized list of files from the csv####
+	#[[ ! -d $lang ]] && mkdir $lang; cd $lang;
+	# while IFS="," read filename
+	# do
+	#     curl http://www.repository.voxforge1.org/downloads/$lang/Trunk/Audio/Main/16kHz_16bit/$filename
+	# done < "${lang}_list.csv"
+	#
+	# cd $rootdir;
 done
 
-# extract files from tgz
+#### Extract files from tgz ####
 for lang in ${LanguageArray[*]}; do
 	cd $rootdir/$lang/www.repository.voxforge1.org/downloads/$lang/Trunk/Audio/Main/16kHz_16bit;
 	for i in *.tgz; do tar -zxvf $i -C $rootdir/$lang; done
 done
 
-# copy .wav files to corresponding language
+#### Copy .wav files to corresponding language  ####
 for lang in ${LanguageArray[*]}; do
 	cd $rootdir;
 	# create a new folder for wav files if not exist
@@ -49,7 +50,7 @@ for lang in ${LanguageArray[*]}; do
 
 		# rename and move all the wav files to one subfolder
 		cd $rootdir/$lang/$folder/wav;
-		for f in *.wav; do mv "$f" "${f}_${folder}.wav";done
+		for f in *.wav; do mv "$f" "${f}_${folder}.wav"; done
 		mv *.wav $rootdir/$newlang
 	done
 done
