127f1a44bc71d1635176dca79a764ad271d38dcb
Bowen Zhang <bzhang193@gmail.com>
2020-08-28 22:14:26 -0400

    small changes to pathing in bash and python scripts

--- run_setup.sh
+++ run_setup.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
+
+#### INSTRUCTIONS: Make sure you set your directory to where you want to download the filesand run the script there ####
 #### This script will download, extract, and move wav files from the voxforge site ####
 
-#### NOTE: Make sure you set your directory to where you want to download the files before running script ####
-#### This script creates the data directory and downloads plus extracts all raw wav files in the following langauges ####
-# List of languages to download. Based on the URL of voxforge.
-LanguageArray=("en" "fr" "es" "de")
+# List of languages to download. Based on the URL, add or delete manually if you wish to change languages to scrape
+LanguageArray=("en" "fr" "es" "de", "it")
 
-# creates a new folder in your current working directory
-[[ ! -d vox_lang ]] && mkdir vox_lang;
-cd vox_lang;
+# Sets working directory as root location to download files to
 rootdir=$(pwd)
 
-### Download files for each language and wait 5 seconds per download to not overload server ####
+#### STEP 1: Download files for each language and wait 5 seconds per download to not overload server #####
 for lang in ${LanguageArray[*]}; do
 
 	#### Use wget instead if you want to just download from top to bottom or everything (-Q limits the max size downloaded per class) ####
@@ -27,13 +25,15 @@ for lang in ${LanguageArray[*]}; do
 	# cd $rootdir;
 done
 
-#### Extract files from tgz ####
+
+#### STEP 2: Extract files from tgz ####
 for lang in ${LanguageArray[*]}; do
 	cd $rootdir/$lang/www.repository.voxforge1.org/downloads/$lang/Trunk/Audio/Main/16kHz_16bit;
 	for i in *.tgz; do tar -zxvf $i -C $rootdir/$lang; done
 done
 
-#### Copy .wav files to corresponding language  ####
+
+#### STEP 3: Copy .wav files to corresponding language  ####
 for lang in ${LanguageArray[*]}; do
 	cd $rootdir;
 	# create a new folder for wav files if not exist
@@ -45,7 +45,7 @@ for lang in ${LanguageArray[*]}; do
 	for folder in *; do
 		# skips the folder with tar files
 		if [[ "$folder" == "www.repository.voxforge1.org" ]]; then
-			echo "if worked and found www.repo folder"
+			continue
 		fi
 
 		# rename and move all the wav files to one subfolder
