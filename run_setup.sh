eaa78befb8a734d01c4c7fd35bc8e47174709684
Bowen Zhang <bzhang193@gmail.com>
2020-09-20 21:17:18 -0400

    updated comments

--- run_setup.sh
+++ run_setup.sh
@@ -13,19 +13,8 @@ rootdir=$(pwd)
 
 #### STEP 1: Download files for each language and wait 5 seconds per download to not overload server #####
 for lang in ${LanguageArray[*]}; do
-
-	#### Use wget instead if you want to just download from top to bottom for everything ####
 	# -Q limits the max size downloaded per class if you wish to not download the whole repo
 	wget -r -np -Q2500m -nc -w3 R "index.html*" -P $rootdir/$lang http://www.repository.voxforge1.org/downloads/$lang/Trunk/Audio/Main/16kHz_16bit/
-
-	#### Use the code below if you want to download a randomized list of files from the csv####
-	#[[ ! -d $lang ]] && mkdir $lang; cd $lang;
-	# while IFS="," read filename
-	# do
-	#     curl http://www.repository.voxforge1.org/downloads/$lang/Trunk/Audio/Main/16kHz_16bit/$filename
-	# done < "${lang}_list.csv"
-	#
-	# cd $rootdir;
 done
 
 
@@ -63,7 +52,7 @@ for lang in ${LanguageArray[*]}; do
 done
 
 
-#### STEP 3: DELETE FOLDERS WE DO NOT NEED ANYMORE ####
+#### STEP 4: DELETE FOLDERS WE DO NOT NEED ANYMORE ####
 # remove all the empty extracted folders we moved the wav files from and all the targz folders we downloaded
 cd $rootdir
 rm -r en fr de it es
